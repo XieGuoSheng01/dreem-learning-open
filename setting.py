a6535a804856ab522c0a2083ea4e661d7ac15bca
XieGuoSheng01 <134775984+XieGuoSheng01@users.noreply.github.com>
2024-12-25 16:20:13 +0800

    Create setting.py

---
+++ setting.py
@@ -0,0 +1,14 @@
+BASE_DIRECTORY = "/data/"
+BASE_DIRECTORY_H5 = BASE_DIRECTORY + "h5/"
+BASE_DIRECTORY_MEMMAP = BASE_DIRECTORY + "memmap/"
+EXPERIMENTS_DIRECTORY = BASE_DIRECTORY + 'experiments/'
+
+DODH_SETTINGS = {
+    'h5_directory': BASE_DIRECTORY_H5 + 'dodh/',
+    'memmap_directory': BASE_DIRECTORY_MEMMAP + 'dodh/'
+}
+
+DODO_SETTINGS = {
+    'h5_directory': BASE_DIRECTORY_H5 + 'dodo/',
+    'memmap_directory': BASE_DIRECTORY_MEMMAP + 'dodo/'
+}
